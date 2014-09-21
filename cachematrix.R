5b05483c096c607cc257f264a87a3e0c105dd6ff
Sharan Karthikeyan <sharankarthikeyan2@gmail.com>
2014-09-21 15:23:47 -0400

    Update cachematrix.R
    This is the assignment for R programing

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is the assignement 2 for "R" programming.
+## This function makes cache matrix
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+ m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+
 
 }
 
 
-## Write a short comment describing this function
+## this is the cacheing data
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+         m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+
 }
